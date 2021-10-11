17b4cfc0f63aca927442fc9b810b903edf196036
Sherifsss <kristaps_mezatucs2@inbox.lv>
2021-10-11 16:38:32 +0200

    Add project files.

---
+++ Compulsory_Assignment1_Model_Book/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Compulsory_Assignment1_Model_Book
+{
+    public class Book
+    {
+
+        public Book()
+        {
+
+        }
+
+        public string ISBN13 { get; set; }
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int PageNr { get; set; }
+
+    }
+}
